3191d643e50f2117c132c067e0ee754b70bc9e6f
Rifqy Iza Fahrizal <rifqyizafahrizal1@gmail.com>
2025-05-08 02:49:21 +0000

    buat agar tampilannya semua terlihat keren, modern, mobile resposive, mobile menu sticky icon, dan cyber color

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ import type { PropsWithChildren } from 'react';
 
 export default function AuthLayout({ children }: PropsWithChildren) {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-secondary p-4">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4">
       {children}
     </div>
   );
